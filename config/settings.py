9be19aa64b022a5c61216b095c5db2a766c425d4
josephrwabuti <josephrwabuti@gmail.com>
2026-06-14 12:59:39 +0300

    Create base apps structure (accounts, orders, dashboard)

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'apps.accounts',
+    'apps.dashboard',
+    'apps.orders',
 ]
 
 MIDDLEWARE = [
